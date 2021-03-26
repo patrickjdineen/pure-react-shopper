614d5c024cfd223e83ae840c7176a7d2f20458a3
Patrick Dineen <patrickjdineen@gmail.com>
2021-03-25 21:12:58 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Nav from './Nav';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+const [activeTab, setActiveTab] = useState('items');
+
+  return(
+    <div className='App'>
+      <Nav 
+      activeTab={activeTab}
+      onTabChange={setActiveTab}
+      />
+      <main className='App-content'>
+        <Content tab={activeTab} />
+      </main>
     </div>
   );
-}
+};
+
+const Content=({ tab })=>{
+  switch (tab){
+    default:
+      case 'items':
+        return <span>the items</span>;
+      case 'cart':
+        return <span>the cart</span>
+  }
+};
 
 export default App;
